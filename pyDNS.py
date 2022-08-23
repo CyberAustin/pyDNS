d8c857e28c874491bfc93e07721601d24e7dc9b9
CyberAustin <austin.y.schibler@gmail.com>
2022-08-22 21:40:04 -0500

    Starting to clean the code up and get it class'd

--- pyDNS.py
+++ pyDNS.py
@@ -1,10 +1,30 @@
 class pyDNS():
 
-    def __init__(self, message):
-        self.message = message
-        self.id = int.from_bytes(message[0:2], "big")
+    def __init__(self, query_packet):
+        self.query_packet = query_packet
+        self.id = int.from_bytes(query_packet[0:2], "big")
+        self.fqdn, self.fqdn_with_count = self.getFQDNs(query_packet)
+        self.packet_type = self.getPacketType(query_packet)
 
-        if int.from_bytes(message[2:3], "big") == 1:
-            self.packet_type = "Query"
+    def getPacketType(self, packet):
+        if int.from_bytes(packet[2:3], "big") == 1:
+            packet_type = "Query"
         else:
-            self.packet_type = "Response"
+            packet_type = "Response"
+        return packet_type
+
+    def getFQDNs(self, packet):
+        i = 12
+        fqdn = ''
+        fqdn_with_count = ''
+        while packet[i] != 0:
+            fqdn_with_count += chr(packet[i])
+            if i == 12:
+                i += 1
+                continue
+            if packet[i] <= 32:
+                fqdn += '.'
+            else:
+                fqdn += chr(packet[i])
+            i += 1
+        return fqdn, fqdn_with_count
